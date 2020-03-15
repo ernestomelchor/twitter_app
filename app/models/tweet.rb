fed4a7c656226001d6e3c9ab63aaebbcab77682f
ernestomelchor <nurturelove88@gmail.com>
2020-03-14 20:35:47 -0500

    add friendly updated_at and created_at to model Tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,11 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+
+  def friendly_updated_at
+    updated_at.strftime("%A, %b %d, %Y at:%l:%M %p")
+  end
+
+  def friendly_created_at
+    created_at..strftime("%A, %b %d, %Y at:%l:%M %p")
+  end
 end
